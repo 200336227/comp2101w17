7f14837eb8df8cb0eb35a919bd867a0bea736cc5
200336227 <200336227@student.georgianc.on.ca>
2017-01-27 12:12:20 -0500

    update

--- lab1/picstats.sh
+++ lab1/picstats.sh
@@ -2,7 +2,7 @@
 # Display regular files in the Pictures directory and size
 
 du -ah ~/Pictures
-
+echo
 
 #Display 3 largest files in the Pictures directory
 echo "List three largest files in the directory."
