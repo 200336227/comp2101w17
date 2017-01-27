d3d4cb8b28a1babd0ff9a0b033fdcf810f2f47ef
200336227 <200336227@student.georgianc.on.ca>
2017-01-27 14:56:00 -0500

    update

---
+++ lab2/welcome.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# This script print out a welcome message
+# IT demonstrates using variables
+
+export MYNAME="Kien Vinh Tran"
+mytitle="Chief Executive Operator"
+myhostname=`hostname`
+dayoftheweek=$(date +%A)
+
+echo "Welcome to planet $myhostname , $mytitle $MYNAME!"
+echo "Today is $dayoftheweek."
+echo
+
+
