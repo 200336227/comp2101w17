6d98134107a2564283e23d8b38b2d3b57961d2fa
200336227 <200336227@student.georgianc.on.ca>
2017-02-10 13:53:04 -0500

    update Feb 10

---
+++ lab2/rollafood.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Create an array containing 11 different food names.
+# Create two variables with random numbers in the from 1-6 each
+# Add the two numbers together and use the sume as an index to display a food from your array
+
+# Create a array of foods
+foods=(apple banana pizza wings beer steak sandwich "pop tart" chicken ribs dirt)
+
+
---
+++ lab2/showinterfaces.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Create an array using the output of ifconfig which has the names of the network interfaces in it. Use those names to extract ip addresses of the interfaces, also using ifconfig output. Also parse the output of route -n to display the ip address of the default gateway.
+
+# Build interfaces array with interface names as elements
+interfaces=( $(ifconfig | grep '^[A-Za-z]' | awk '{print $1}') )
+
+# Find the ip address of the interfaces
+ip0=$(ifconfig ${interfaces[0]} | sed -n '/inet addr:/s/.*addr:\([0-9.][0-9.]*\).*/\1/p')
+ip1=$(ifconfig ${interfaces[1]} | sed -n '/inet addr:/s/.*addr:\([0-9.][0-9.]*\).*/\1/p')
+
+# Extract default gateway ip from the route table
+gw=$(route -n | awk '/^0.0.0.0/{print $2}')
+
+echo "Interface ${interfaces[0]} has ip address $ip0"
+echo "Interface ${interfaces[1]} has ip address $ip1"
+echo "My default gateway is $gw"
+
+
