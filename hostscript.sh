1789e8baf8308368a04afe3d4d3bdfd805a05f09
200336227 <200336227@student.georgianc.on.ca>
2017-02-24 17:20:22 -0500

    Update

--- hostscript.sh
+++ hostscript.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Main Menu for User Choose Options
 
-echo "Hello"
+echo "Hello. Today is `date`"
 PS3='Please enter the options you are looking for: '
-options=("System Name" "Domain Name" "IP address" "OS name" "OS version" "Quit")
+options=("System Name" "Domain Name" "IP address" "OS name" "OS version" "CPU description" "Memory installed" "Disk space" "Printers" "Software" "Quit")
 
 # For hostname
 host=$(hostname)
@@ -19,6 +19,22 @@ ip0=$(ifconfig ${interfaces} | sed -n '/inet addr:/s/.*addr:\([0-9.][0-9.]*\).*/
 osname=$(uname -mrs | awk '{print $1}')
 osversion=$(uname -mrs | awk '{print $2}')
 
+# For CPU specification
+cpu=$(lscpu | sed -n '2p;13p;15p' && printf \n)
+
+# For Memory installed
+mem=$(free -h | sed -n '1,2p')
+
+# For available disk space
+disk=$(df -h | sed -n '1p;4p')
+
+# For List of installed printers
+print=$(lpstat -p | awk '{ print $2}')
+
+# For list of installed software
+soft=$(cat /var/lib/apt/extended_states)
+
+# Main script
 select opt in "${options[@]}"
 do
 	case $opt in
@@ -52,6 +68,34 @@ do
 			echo "You choose option 5"
 			sleep 3
 			echo "The current OS version is $osversion"
+			;;
+		"CPU description")
+			echo "You choose option 6"
+			sleep 3
+			echo -e "The current system's CPU specification\n$cpu"
+			;;
+		"Memory installed")
+			echo "You choose option 7"
+			sleep 3
+			echo -e "Installed memory of the current system is\n$mem"
+			;;
+		"Disk space")
+			echo "You choose option 8"
+			sleep 3
+			echo -e "Disk space of the current system is\n$disk"
+			;;
+		"Printers")
+			echo "You choose option 9"
+			sleep 3
+			if [ -z $print ]
+				then echo "There is no printers install on this system"
+				else echo "The printers for this system is/are $print"
+			fi			
+			;;
+		"Software")
+			echo "You choose option 10"
+			sleep 3
+			echo -e "The current system has installed following software\n$soft"
 			;;
 		"Quit")
 			break
---
+++ script.sh
@@ -0,0 +1,11 @@
+function help ( )
+{
+	printf "\n"
+	printf
+	printf
+}
+
+function help-and-exit { help ; exit }
+
+source filename.sh : running command from other files
+
